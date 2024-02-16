2bb12187704d201cc19cbbbf00bc5d7717e7b41b
Boroda9992 <fedko.ak@gmail.com>
2024-02-16 21:42:37 +0300

    placeholder

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] weights = {
+            90, 91, 91, 92, 91, 89, 88, 86, 0, 0, 0, 0
+        } ;
+        weights[0] = 90;
+        int januaryWeight = weights[0];
+        System.out.println(januaryWeight);
+        System.out.println(weights[0]);
+        System.out.println(weights[4]);
+//        for (int i = 0; i < weights.length; i++) {
+//            System.out.println(weights[i]);
+        System.out.println(Arrays.toString(weights));
+
+
     }
 }
